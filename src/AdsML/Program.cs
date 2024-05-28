a658ecbc8efcabd6c9e1c6ed3d99005f5dce31f1
mhshahmoradi <mh.shahmorady@gmail.com>
2024-05-28 19:55:42 +0330

    add AppOption service

--- src/AdsML/Program.cs
+++ src/AdsML/Program.cs
@@ -1,9 +1,13 @@
 using AdsML.Common.Extensions;
+using AdsML.Common.Shared;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddOptions<AppSetting>()
+    .Bind(builder.Configuration.GetSection(AppSetting.ConfigurationSectionName));
 
 builder.Services.ConfigureDbContexts(builder.Configuration);
 builder.Services.ConfigureValidator();
